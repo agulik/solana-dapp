d2d886902287e22d3a896879c46e160f4904fac9
Andrew Gulik <andrewgulik@gmail.com>
2021-11-07 20:44:04 -0500

    start solana program

--- programs/solana-app/src/lib.rs
+++ programs/solana-app/src/lib.rs
@@ -6,9 +6,26 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_app {
   use super::*;
   pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> ProgramResult {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+
+    base_account.total_images = 0;
     Ok(())
   }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_images: u64,
+}
